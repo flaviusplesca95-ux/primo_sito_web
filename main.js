218100c39e0e9d5866c0aa51bcbefbc55e73dc9e
flaviusplesca95-ux <flaviusplesca95@gmail.com>
2026-06-09 14:24:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let tit = document.getElementById ("title")
+let btn = document.getElementById ("btn")
+
+btn.addEventListener("click",function() { 
+    tit.innerHTML = "Ciao sono stato modificato "   }
+)
+
